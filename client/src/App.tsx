6d42eddc9a747b6ce20650e1afe404b3f743c098
Ashish Mittal <aashishrbmittal@gmail.com>
2026-03-10 21:32:11 +0530

    Browser Router Removed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,10 @@ import CaseStudyDetail from "./pages/CaseStudyDetail";
 function App() {
   return (
     <>
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/case-studies/:slug" element={<CaseStudyDetail />} />
         </Routes>
-      </BrowserRouter>
     </>
   )
 }
