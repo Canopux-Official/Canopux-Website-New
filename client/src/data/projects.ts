e52b4ae5183688131b04053d243fa0dbec97e2dd
Canopux-Official <canopus.incglobe@gmail.com>
2026-07-03 18:09:34 +0530

    Update projects.ts

--- client/src/data/projects.ts
+++ client/src/data/projects.ts
@@ -87,7 +87,7 @@ export const projects: Project[] = [
 
     impact: "A comprehensive and dynamic educational management system enhancing student and admin experiences",
 
-    liveURL: "https://sms-canopux-18ui.vercel.app/",
+    liveURL: "https://sms.canopux.org/",
 
     purpose:
       "Student Management System project is designed to streamline educational administration while providing students with easy access to learning materials and personal academic records. The system enables super admins to control features, admins to manage students and sessions, and students to track their performance in a secure, progressive web application.",
